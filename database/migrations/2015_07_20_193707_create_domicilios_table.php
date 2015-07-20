b2d11620dc68fe19a32e24713b97f0efaef7acb4
Omar Garcia <isc.omargarcia@gmail.com>
2015-07-20 14:41:44 -0500

    Agregadas las migraciones para modulo de Domicilios

---
+++ database/migrations/2015_07_20_193707_create_domicilios_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDomiciliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('domicilios', function($table){
+            $table->increments('id');
+            $table->string('calle', 45);
+            $table->string('localidad', 45);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('domicilios');
+    }
+}
