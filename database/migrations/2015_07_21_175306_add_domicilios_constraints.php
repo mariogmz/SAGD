c9bc911c43dac697a75695da849c06d2c9f9fac1
Mario Gómez <mariogomezmtz@gmail.com>
2015-07-21 13:21:54 -0500

    Agregada migracion de domicilios.

---
+++ database/migrations/2015_07_21_175306_add_domicilios_constraints.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDomiciliosConstraints extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('domicilios', function ($table)
+        {
+            $table->integer('codigo_postal_id')->unsigned();
+            $table->foreign('codigo_postal_id')->references('id')->on('codigos_postales');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('domicilios', function ($table)
+        {
+            $table->dropForeign('domicilios_codigo_postal_id_foreign');
+            $table->dropColumn('codigo_postal_id');
+        });
+    }
+}
