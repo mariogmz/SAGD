dec6afc80464fbd8984af1a8f51d03bdfdedacee
Mario Gómez <mariogomezmtz@gmail.com>
2015-07-28 15:20:56 -0500

    Agregadas pruebas de modelo de configuraciones sucursales.

--- app/SucursalConfiguracion.php
+++ app/SucursalConfiguracion.php
@@ -2,11 +2,50 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Validator;
 
+class SucursalConfiguracion extends LGGModel {
 
-class SucursalConfiguracion extends LGGModel
-{
-    //
+    protected $table = 'sucursales_configuraciones';
+    public $timestamps = false;
+
+    protected $fillable = ['valor_numero', 'valor_texto', 'sucursal_id', 'configuracion_id'];
+    public static $rules = [
+        'valor_numero' => 'numeric|required_without:valor_texto',
+        'valor_texto'  => 'string|required_without:valor_numero'
+    ];
+
+    /**
+     * Define the model hooks
+     * @codeCoverageIgnore
+     */
+    public static function boot()
+    {
+        SucursalConfiguracion::creating(function ($sucursal_configuracion)
+        {
+            if (!$sucursal_configuracion->isValid() || !(empty($sucursal_configuracion->valor_numero) && empty($sucursal_configuracion->valor_texto)))
+            {
+                return false;
+            }
+
+            return true;
+        });
+    }
+
+    /**
+     * Obtiene la sucursal asociada al valor de la configuracion
+     * @returns App\Sucursal
+     */
+    public function sucursal()
+    {
+        return $this->belongsTo('App\Sucursal');
+    }
+
+    /**
+     * Obtiene la sucursal asociada al valor de la configuracion
+     * @returns App\Configuracion
+     */
+    public function configuracion()
+    {
+        return $this->belongsTo('App\Configuracion');
+    }
 }
---
+++ database/factories/SucursalConfiguracionFactory.php
@@ -0,0 +1,49 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+$factory->define(App\SucursalConfiguracion::class, function ($faker)
+{
+    return [
+        'valor_numero'     => null,
+        'valor_texto'      => null,
+        'sucursal_id'      => factory(App\Sucursal::class)->create()->id,
+        'configuracion_id' => factory(App\Configuracion::class)->create()->id
+    ];
+});
+
+$factory->defineAs(App\SucursalConfiguracion::class, 'valornumero', function ($faker) use ($factory)
+{
+    $sucursal_configuracion = $factory->raw(App\SucursalConfiguracion::class);
+
+    return array_merge($sucursal_configuracion, [
+        'valor_numero' => $faker->randomFloat(),
+    ]);
+});
+
+$factory->defineAs(App\SucursalConfiguracion::class, 'valortexto', function ($faker) use ($factory)
+{
+    $sucursal_configuracion = $factory->raw(App\SucursalConfiguracion::class);
+
+    return array_merge($sucursal_configuracion, [
+        'valor_texto' => $faker->text(),
+    ]);
+});
+
+$factory->defineAs(App\SucursalConfiguracion::class, 'ambosvalores', function ($faker) use ($factory)
+{
+    $sucursal_configuracion = $factory->raw(App\SucursalConfiguracion::class);
+
+    return array_merge($sucursal_configuracion, [
+        'valor_numero' => $faker->randomFloat(),
+        'valor_texto'  => $faker->text()
+    ]);
+});
