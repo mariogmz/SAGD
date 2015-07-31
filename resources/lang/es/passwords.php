3eab2a26dc1ef65841f1d7ad76a5ff0defc1f99e
Omar Garcia <isc.omargarcia@gmail.com>
2015-07-31 11:30:45 -0500

    Cambiado el lenguaje a ES, agregado traducciones

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña debe tener al menos seis caracteres y ser igual al de confirmacion',
+    'user' => "No pudimos encontrar un usuario con ese correo electrónico",
+    'token' => 'Este token de password reset es invalido',
+    'sent' => 'El link para reestablecer su contraseña ha sido enviado!',
+    'reset' => 'Su contraseña ha sido reestablecida!',
+
+];
