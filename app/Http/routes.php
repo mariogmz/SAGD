1eb56473933e692e2786ee79acbe2eec5b39afdc
Omar Garcia <isc.omargarcia@gmail.com>
2015-08-19 10:59:19 -0500

    Agregado endpoints para obtener token y listado de empleados

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => 'Api', 'prefix' => 'api'], function(){
+    Route::group(['namespace' => 'V1', 'prefix' => 'v1'], function(){
+        Route::resource('authenticate', 'AuthenticateController');
+        Route::post('authenticate', 'AuthenticateController@authenticate');
+
+        Route::resource('empleado', 'EmpleadoController', ['only' => ['index']]);
+    });
+});
