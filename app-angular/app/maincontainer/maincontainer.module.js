d3f33e5caaf5e81e5b71699014abb5d3f03b0468
Mario Gómez <mariogomezmtz@gmail.com>
2015-08-27 11:00:50 -0500

    Borrados archivos innecesarios.

--- app-angular/app/maincontainer/maincontainer.module.js
+++
@@ -1,9 +0,0 @@
-// app/navbar/maincontainer.module.js
-
-(function() {
-    'use strict';
-
-    angular.module('sagdApp.navbar', [
-      'sagdApp.core'
-    ]);
-})();
--- app-angular/public/js/all.js
+++ app-angular/public/js/all.js
@@ -49,16 +49,6 @@
     ]);
 })();
 
-// app/navbar/maincontainer.module.js
-
-(function() {
-    'use strict';
-
-    angular.module('sagdApp.navbar', [
-      'sagdApp.core'
-    ]);
-})();
-
 // app/navbar/navbar.module.js
 
 (function() {
@@ -304,84 +294,6 @@
         vm.errores = error;
       });
     }
-  }
-
-})();
-
-// app/navbar/maincontainer.controller.js
-
-(function () {
-
-  'use strict';
-
-  angular
-    .module('sagdApp.navbar')
-    .controller('NavbarController', NavbarController)
-    .directive('navBar', function () {
-      return {
-        templateUrl: 'app/navbar/navbar.html'
-      };
-    });
-
-  NavbarController.$inject = ['$auth', 'session'];
-
-  function NavbarController($auth, session) {
-    var vm = this;
-    vm.modules = [
-      {
-        nombre: 'Inicio',
-        state: 'home',
-        active: true
-      }, {
-        nombre: 'Productos',
-        state: 'producto',
-        active: false
-      }, {
-        nombre: 'Inventario',
-        state: 'inventario',
-        active: false
-      }, {
-        nombre: 'Ventas',
-        state: 'venta',
-        active: false
-      }, {
-        nombre: 'Clientes',
-        state: 'cliente',
-        active: false
-      }, {
-        nombre: 'Proveedores',
-        state: 'proveedor',
-        active: false
-      }, {
-        nombre: 'Soporte',
-        state: 'soporte',
-        active: false
-      }, {
-        nombre: 'Empleados',
-        state: 'empleado',
-        active: false
-      }, {
-        nombre: 'Cajas y Cortes',
-        state: 'caja_corte',
-        active: false
-      }, {
-        nombre: 'Paqueterías',
-        state: 'paqueteria',
-        active: false
-      }, {
-        nombre: 'Facturación',
-        state: 'facturacion',
-        active: false
-      }, {
-        nombre: 'Configuración',
-        state: 'configuracion',
-        active: false
-      }
-    ];
-
-    vm.isAuthenticated = session.isAuthenticated;
-    vm.logout = session.logout;
-    vm.empleado = session.getEmpleado();
   }
 
 })();
