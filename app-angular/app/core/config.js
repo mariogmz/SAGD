0988ee828ff120a892498d1629ff95786d075808
Mario Gómez <mariogomezmtz@gmail.com>
2015-09-18 16:27:26 -0500

    Encontrado fix para html5mode y rutas, agregadas rutas absolutas en resources del index.

--- app-angular/app/core/config.js
+++ app-angular/app/core/config.js
@@ -8,19 +8,23 @@
     .config(configure)
     .run(updateState);
 
-  configure.$inject = ['$urlRouterProvider', '$authProvider'];
+  configure.$inject = ['$urlRouterProvider', '$authProvider', '$locationProvider'];
 
-  function configure($urlRouterProvider, $authProvider){
+  function configure($urlRouterProvider, $authProvider, $locationProvider){
     var baseUrl = "http://api.sagd.app/api/v1";
     $authProvider.loginUrl = baseUrl + '/authenticate';
     $authProvider.withCredentials = true;
 
     $urlRouterProvider.otherwise('/');
+
+    if (window.history && window.history.pushState) {
+      $locationProvider.html5Mode(true).hashPrefix('!');
+    }
   }
 
-  updateState.$inject = ['$rootScope', 'state'];
+  updateState.$inject = ['$rootScope', '$state', 'state'];
 
-  function updateState($rootScope, state){
+  function updateState($rootScope, $state, state){
     $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams){
       state.setNewState(fromState.name, toState.name);
     });
