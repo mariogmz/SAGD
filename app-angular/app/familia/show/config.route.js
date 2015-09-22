041e52f7177b72c69557970a22c1166a49bee7ca
Mario Gómez <mariogomezmtz@gmail.com>
2015-09-22 15:44:02 -0500

    Agregado create para margen.

---
+++ app-angular/app/familia/show/config.route.js
@@ -0,0 +1,22 @@
+// app/marca/show/config.route.js
+
+(function() {
+    'use strict';
+
+    angular
+        .module('sagdApp.marca')
+        .config(configureRoutes);
+
+    configureRoutes.$inject = ['$stateProvider'];
+
+    function configureRoutes($stateProvider) {
+        $stateProvider
+            .state('marcaShow', {
+                url: 'marca/:id',
+                parent: 'marca',
+                templateUrl: 'app/marca/show/show.html',
+                controller: 'marcaShowController',
+                controllerAs: 'vm'
+            });
+    }
+})();
---
+++ app-angular/app/margen/new/margenNew.controller.js
@@ -0,0 +1,74 @@
+// app/margen/margen.controller.js
+
+(function (){
+
+  'use strict';
+
+  angular
+    .module('sagdApp.margen')
+    .controller('margenNewController', MargenNewController);
+
+  MargenNewController.$inject = ['$auth', '$state', 'api', 'pnotify'];
+
+  function MargenNewController($auth, $state, api, pnotify){
+    if (!$auth.isAuthenticated()) {
+      $state.go('login', {});
+    }
+
+    var vm = this;
+    vm.fields = [
+      {
+        type: 'input',
+        key: 'nombre',
+        templateOptions: {
+          type: 'text',
+          label: 'Nombre:',
+          placeholder: 'Máximo 45 caracteres',
+          required: true
+        }
+      }, {
+        type: 'input',
+        key: 'valor',
+        templateOptions: {
+          type: 'text',
+          label: 'Valor:',
+          placeholder: 'Porcentaje. [0.00 - 1.00]',
+          required: true
+        }
+      }, {
+        type: 'input',
+        key: 'valor_webservice_p1',
+        templateOptions: {
+          type: 'text',
+          label: 'Webservice P1:',
+          placeholder: 'Porcentaje. [0.00 - 1.00]',
+          required: true
+        }
+      }, {
+        type: 'input',
+        key: 'valor_webservice_p8',
+        templateOptions: {
+          type: 'text',
+          label: 'Webservice P8:',
+          placeholder: 'Porcentaje. [0.00 - 1.00]',
+          required: true
+        }
+      }
+    ];
+
+    vm.create = create;
+
+    function create(){
+      api.post('/margen', vm.margen)
+        .then(function (response){
+          debugger;
+          pnotify.alert('¡Exito!', response.data.message, 'success');
+          $state.go('margenShow', {id: response.data.margen.id});
+        })
+        .catch(function (response){
+          pnotify.alertList(response.data.message, response.data.error, 'error');
+        });
+    }
+  }
+
+})();
