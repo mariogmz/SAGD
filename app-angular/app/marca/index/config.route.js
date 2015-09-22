9fba4c9999152af4733855c2616589ee51d4f878
Mario Gómez <mariogomezmtz@gmail.com>
2015-09-22 12:06:57 -0500

    Agregado crud de marca.

---
+++ app-angular/app/marca/index/config.route.js
@@ -0,0 +1,22 @@
+// app/marca/index/config.route.js
+
+(function() {
+    'use strict';
+
+    angular
+        .module('sagdApp.marca')
+        .config(configureRoutes);
+
+    configureRoutes.$inject = ['$stateProvider'];
+
+    function configureRoutes($stateProvider) {
+        $stateProvider
+            .state('marcaIndex', {
+                url: 'marca',
+                parent: 'marca',
+                templateUrl: 'app/marca/index/index.html',
+                controller: 'marcaIndexController',
+                controllerAs: 'vm'
+            });
+    }
+})();
---
+++ app-angular/app/marca/show/marcaShow.controller.js
@@ -0,0 +1,52 @@
+// app/marca/show/marca.controller.js
+
+(function (){
+
+  'use strict';
+
+  angular
+    .module('sagdApp.marca')
+    .controller('marcaShowController', MarcaShowController);
+
+  MarcaShowController.$inject = ['$auth', '$state', '$stateParams', 'api'];
+
+  function MarcaShowController($auth, $state, $stateParams, api){
+    if (!$auth.isAuthenticated()) {
+      $state.go('login', {});
+    }
+
+    var vm = this;
+    vm.id = $stateParams.id;
+    vm.fields = [
+      {
+        type: 'input',
+        key: 'nombre',
+        templateOptions: {
+          type: 'text',
+          label: 'Nombre:'
+        }
+      }
+    ];
+    initialize();
+
+    function initialize(){
+      return obtenerMarca().then(function (response){
+        console.log(response.message);
+      });
+    }
+
+    function obtenerMarca(){
+      return api.get('/marca/', vm.id)
+        .then(function (response){
+          vm.marca = response.data.marca;
+          return response.data;
+        })
+        .catch(function (response){
+          vm.error = response.data;
+          return response.data;
+        });
+    }
+  }
+
+})
+();
