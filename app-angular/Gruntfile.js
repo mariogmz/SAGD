781ec8e9ca6105db46cf0032e393b51b1977f9fa
Mario Gómez <mariogomezmtz@gmail.com>
2015-09-22 14:56:24 -0500

    Agregado modulo non-block de pnotify.

--- app-angular/Gruntfile.js
+++ app-angular/Gruntfile.js
@@ -21,6 +21,7 @@ module.exports = function(grunt) {
           './bower_components/pnotify/src/pnotify.core.js',
           './bower_components/pnotify/src/pnotify.desktop.js',
           './bower_components/pnotify/src/pnotify.buttons.js',
+          './bower_components/pnotify/src/pnotify.nonblock.js',
           './bower_components/pnotify/src/pnotify.callbacks.js'
         ],
         dest: './public/libs/frontend.js'
@@ -68,6 +69,7 @@ module.exports = function(grunt) {
           './bower_components/pnotify/src/pnotify.core.js',
           './bower_components/pnotify/src/pnotify.desktop.js',
           './bower_components/pnotify/src/pnotify.buttons.js',
+          './bower_components/pnotify/src/pnotify.nonblock.js',
           './bower_components/pnotify/src/pnotify.callbacks.js'
         ],
         tasks: ['concat:libs', 'uglify:libs'],
