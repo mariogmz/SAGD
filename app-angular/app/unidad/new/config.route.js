a7ccb0c33331c36a2ebbc7e7d8b3d24b9065068e
Omar Garcia <isc.omargarcia@gmail.com>
2015-09-23 09:49:07 -0500

    Agregado el new de unidades

---
+++ app-angular/app/unidad/new/config.route.js
@@ -0,0 +1,22 @@
+// app/unidad/new/config.route.js
+
+(function() {
+    'use strict';
+
+    angular
+        .module('sagdApp.unidad')
+        .config(configureRoutes);
+
+    configureRoutes.$inject = ['$stateProvider'];
+
+    function configureRoutes($stateProvider) {
+        $stateProvider
+            .state('unidadNew', {
+                url: 'unidad/nueva',
+                parent: 'unidad',
+                templateUrl: 'app/unidad/new/new.html',
+                controller: 'unidadNewController',
+                controllerAs: 'vm'
+            });
+    }
+})();
---
+++ app-angular/app/unidad/new/new.controller.js
@@ -0,0 +1,62 @@
+// app/unidad/unidad.controller.js
+
+(function() {
+  'use strict';
+
+  angular
+    .module('sagdApp.unidad')
+    .controller('unidadNewController', unidadNewController);
+
+  unidadNewController.$inject = ['$auth', '$state', 'api', 'pnotify'];
+
+  /* @ngInject */
+  function unidadNewController($auth, $state, api, pnotify) {
+    if (!$auth.isAuthenticated()) {
+      $state.go('login', {});
+    }
+
+    var vm = this;
+
+    vm.create = create;
+
+    vm.fields = [
+      {
+        type: 'input',
+        key: 'clave',
+        templateOptions: {
+          type: 'text',
+          label: 'Clave:',
+          placeholder: 'Máximo 4 letras',
+          required: true
+        }
+      }, {
+        type: 'input',
+        key: 'nombre',
+        templateOptions: {
+          type: 'text',
+          label: 'Nombre:',
+          placeholder: 'Máximo 45 caracteres',
+          required: true
+        }
+      }
+    ];
+
+    activate();
+
+    ////////////////
+
+    function activate() {
+    }
+
+    function create() {
+      api.post('/unidad', vm.unidad)
+        .then(function(response){
+          pnotify.alert('Exito', response.data.message, 'success');
+          $state.go('unidadShow', {id: response.data.unidad.id});
+        })
+        .catch(function(response){
+          pnotify.alertList(response.data.message, response.data.error, 'error');
+        });
+    }
+  }
+})();
