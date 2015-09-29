5efdc2a3f4bb8aaad10bda040f999671e1995c6e
Mario Gómez <mariogomezmtz@gmail.com>
2015-09-29 13:34:34 -0500

    Agregadas llamadas a recursos para catálogos.

--- app-angular/app/producto/new/productoNew.controller.js
+++ app-angular/app/producto/new/productoNew.controller.js
@@ -8,9 +8,9 @@
     .module('sagdApp.producto')
     .controller('productoNewController', ProductoNewController);
 
-  ProductoNewController.$inject = ['$auth', '$state'];
+  ProductoNewController.$inject = ['$auth', '$state', 'api'];
 
-  function ProductoNewController($auth, $state){
+  function ProductoNewController($auth, $state, api){
     if (!$auth.isAuthenticated()) {
       $state.go('login', {});
     } else {
@@ -19,6 +19,53 @@
 
     var vm = this;
     vm.back = goBack;
+
+    initialize();
+
+    /**
+     * Resource calls on call order
+     */
+    function obtenerMarcas(){
+      return api.get('/marca').then(function (response){
+        vm.marcas = response.data;
+        console.log('Marcas obtenidas correctamente');
+      });
+    }
+
+    function obtenerSubfamilias(){
+      return obtenerMarcas().then(function (){
+        return api.get('/subfamilia').then(function (response){
+          vm.subfamilias = response.data;
+          console.log('Subfamilias obtenidas');
+
+        });
+      });
+    }
+
+    function obtenerUnidades(){
+      return obtenerSubfamilias().then(function (){
+        return api.get('/unidad').then(function (response){
+          vm.unidades = response.data;
+          console.log('Unidades obtenidas correctamente');
+
+        });
+      });
+    }
+
+    function obtenerTiposDeGarantias(){
+      return obtenerUnidades().then(function (){
+        return api.get('/tipo-garantia').then(function (response){
+          vm.tiposGarantia = response.data;
+          console.log('Tipos de garantía obtenidos correctamente');
+        });
+      });
+    }
+
+    function initialize(){
+      obtenerTiposDeGarantias().then(function (){
+        console.log('Datos obtenidos correctamente');
+      });
+    }
 
     function goBack(){
       window.history.back();
