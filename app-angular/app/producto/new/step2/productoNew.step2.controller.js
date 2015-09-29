7f658fc6b4465dbfa7745bd8bbb21ce323b1bddb
Mario Gómez <mariogomezmtz@gmail.com>
2015-09-29 11:43:51 -0500

    Agregados directorios para dividir la creacion de un nuevo producto en dos pasos.

---
+++ app-angular/app/producto/new/step2/productoNew.step2.controller.js
@@ -0,0 +1,17 @@
+// app/producto/new/step2/productoNew.step2.controller.js
+
+(function (){
+
+  'use strict';
+
+  angular
+    .module('sagdApp.producto')
+    .controller('productoNewStep2Controller', ProductoNewStep2Controller);
+
+  ProductoNewStep2Controller.$inject = [];
+
+  function ProductoNewStep2Controller(){
+
+  }
+
+})();
