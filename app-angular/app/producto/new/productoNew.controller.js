9b6a79ebdec073b353a2ae21c5d85a11b871241e
Mario Gómez <mariogomezmtz@gmail.com>
2015-09-30 17:51:28 -0500

    Inicio de implementación de posts para distintos modelos, queda pendiente el alta de producto-sucursal.

--- app-angular/app/producto/new/productoNew.controller.js
+++ app-angular/app/producto/new/productoNew.controller.js
@@ -8,9 +8,9 @@
     .module('sagdApp.producto')
     .controller('productoNewController', ProductoNewController);
 
-  ProductoNewController.$inject = ['$auth', '$state', 'api'];
+  ProductoNewController.$inject = ['$auth', '$state', 'api', 'pnotify'];
 
-  function ProductoNewController($auth, $state, api){
+  function ProductoNewController($auth, $state, api, pnotify){
     if (!$auth.isAuthenticated()) {
       $state.go('login', {});
     } else {
@@ -21,7 +21,12 @@
     vm.producto = {};
     vm.precio = {};
     vm.dimension = {};
+
     vm.back = goBack;
+    vm.updateSubclave = updateSubclave;
+    vm.updateClave = updateClave;
+    vm.save = crearPrecio;
+
     initialize();
 
     /**
@@ -72,9 +77,66 @@
       });
     }
 
+    function obtenerProveedoresConSucursales(){
+      return api.get('/proveedor')
+    }
+
+    function updateSubclave(){
+      if (vm.producto) {
+        vm.producto.subclave = vm.producto.subclave || vm.producto.numero_parte;
+        vm.producto.subclave = vm.producto.subclave.toUpperCase();
+        updateClave();
+      }
+    }
+
+    function updateClave(){
+      var subfamilia = vm.subfamilia ? vm.subfamilia.clave : '';
+      var familia = vm.subfamilia ? vm.subfamilia.familia.clave : '';
+      var marca = vm.marca ? vm.marca.clave : '';
+
+      vm.producto.clave = familia + subfamilia + marca + (vm.producto.subclave || '');
+      vm.producto.subfamilia_id = vm.subfamilia ? vm.subfamilia.id : null;
+      vm.producto.marca_id = vm.marca ? vm.marca.id : null;
+    }
+
     function initialize(){
       obtenerMargenes().then(function (){
         console.log('Datos obtenidos correctamente');
+      });
+    }
+
+    function crearProducto(){
+      return api.post('/producto', vm.producto)
+        .then(function (response){
+          vm.producto = response.data.producto;
+          console.log('Producto creado correctamente, id: ' + vm.producto.id);
+        }).catch(function (response){
+          pnotify.alertList(response.data.message, response.data.error, 'error');
+        });
+    }
+
+    function crearDimension(){
+      return crearProducto().then(function (){
+        return api.post('/dimension', vm.dimension)
+          .then(function (response){
+            vm.dimension = response.data.dimension;
+            console.log('Dimension creada correctamente, id: ' + vm.dimension.id);
+          })
+          .catch(function (response){
+            pnotify.alertList(response.data.message, response.data.error, 'error');
+          });
+      });
+    }
+
+    function crearProductoSucursal(){
+      return crearDimension().then(function (){
+
+      });
+    }
+
+    function crearPrecio(){
+      return crearProductoSucursal().then(function(){
+
       });
     }
 
