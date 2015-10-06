0433e92aaf2cdce01222091d004e12ed821016a6
= <=>
2015-10-06 13:33:51 -0500

    Refactorizar selectbox, orden alfabético y eliminar controller inline

--- app-angular/app/cliente/new/clienteNew.controller.js
+++ app-angular/app/cliente/new/clienteNew.controller.js
@@ -52,30 +52,19 @@
         key: 'cliente_referencia_id',
         templateOptions: {
           label: 'Referencia:',
-          required: true,
           options: [],
-          ngOptions: 'clientes_referencias.id as clientes_referencias.nombre for clientes_referencias in to.options'
-        },
-        controller: /* @ngInject */ function ($scope){
-          $scope.to.loading = api.get('/cliente-referencia').then(function (response){
-            $scope.to.options = response.data;
-            return response;
-          });
+          ngOptions: 'clientes_referencias.id as clientes_referencias.nombre for clientes_referencias in to.options | orderBy:"nombre"',
+          required: true,
         }
+
       }, {
         type: 'select',
         key: 'rol_id',
         templateOptions: {
           label: 'Rol:',
-          required: true,
           options: [],
-          ngOptions: 'roles.id as roles.nombre for roles in to.options'
-        },
-        controller: /* @ngInject */ function ($scope){
-          $scope.to.loading = api.get('/rol').then(function (response){
-            $scope.to.options = response.data;
-            return response;
-          });
+          ngOptions: 'roles.id as roles.nombre for roles in to.options | orderBy:"nombre"',
+          required: true,
         }
       }, {
         type: 'select',
@@ -84,13 +73,7 @@
           label: 'Estatus:',
           required: true,
           options: [],
-          ngOptions: 'clientes_estatus.id as clientes_estatus.nombre for clientes_estatus in to.options'
-        },
-        controller: /* @ngInject */ function ($scope){
-          $scope.to.loading = api.get('/cliente-estatus').then(function (response){
-            $scope.to.options = response.data;
-            return response;
-          });
+          ngOptions: 'clientes_estatus.id as clientes_estatus.nombre for clientes_estatus in to.options | orderBy:"nombre"'
         }
       }, {
         type: 'select',
@@ -99,17 +82,32 @@
           label: 'Sucursal de preferencia:',
           required: true,
           options: [],
-          ngOptions: 'sucursales.id as sucursales.nombre for sucursales in to.options'
-        },
-        controller: /* @ngInject */ function ($scope){
-          $scope.to.loading = api.get('/sucursal').then(function (response){
-            $scope.to.options = response.data;
-            return response;
-          });
+          ngOptions: 'sucursales.id as sucursales.nombre for sucursales in to.options | orderBy:"id"'
         }
       }
 
     ];
+
+
+    activate();
+
+    function activate() {
+
+      return obtenerReferencias()
+        .then(function (response) {
+          return obtenerRoles()
+            .then(function (response) {
+              return obtenerEstatus()
+                .then(function (response) {
+                  return obtenerSucursales()
+                    .then(function (response) {
+                      assignFields();
+                    });
+                });
+            });
+        });
+    }
+
 
     function onSubmit(){
       return api.post('/cliente', vm.model)
@@ -123,6 +121,80 @@
             pnotify.alertList('No se pudo guardar el cliente', vm.error.error, 'error');
             return response;
           });
+    }
+
+    function obtenerReferencias() {
+      return api.get('/cliente-referencia')
+          .then(function (response) {
+            vm.referencias = response.data;
+            return response;
+          })
+          .catch(function (response) {
+            vm.error = response.data;
+            pnotify.alert('Hubo un problema al obtener las Referencias', vm.error.error, 'error');
+            return response;
+          });
+    }
+
+    function obtenerRoles() {
+      return api.get('/rol')
+          .then(function (response) {
+            vm.roles = response.data;
+            return response;
+          })
+          .catch(function (response) {
+            vm.error = response.data;
+            pnotify.alert('Hubo un problema al obtener los Roles', vm.error.error, 'error');
+            return response;
+          });
+    }
+
+    function obtenerEstatus() {
+      return api.get('/cliente-estatus')
+          .then(function (response) {
+            vm.estatus = response.data;
+            return response;
+          })
+          .catch(function (response) {
+            vm.error = response.data;
+            pnotify.alert('Hubo un problema al obtener los Estatus', vm.error.error, 'error');
+            return response;
+          });
+    }
+
+    function obtenerSucursales() {
+      return api.get('/sucursal')
+          .then(function (response) {
+            vm.sucursales = response.data;
+            return response;
+          })
+          .catch(function (response) {
+            vm.error = response.data;
+            pnotify.alert('Hubo un problema al obtener las Sucursales', vm.error.error, 'error');
+            return response;
+          });
+    }
+
+    function assignFields() {
+      vm.fields = vm.fields.map(function(object) {
+        if(object.key == "cliente_referencia_id") {
+          object.templateOptions.options = vm.referencias;
+        }
+
+        if(object.key == "rol_id") {
+          object.templateOptions.options = vm.roles;
+        }
+
+        if(object.key == "cliente_estatus_id") {
+          object.templateOptions.options = vm.estatus;
+        }
+
+        if(object.key == "sucursal_id") {
+          object.templateOptions.options = vm.sucursales;
+        }
+
+        return object;
+      });
     }
 
   }
