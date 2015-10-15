f28a5a71cdbc1a675fc32bd8856dc1aa6c3eeb70
Omar Garcia <ogarcia@zegucom.com.mx>
2015-10-15 12:33:16 -0500

    Refactorizacion del servidor de nodejs para delegar tareas y limpiar codigo

--- socket.js
+++ socket.js
@@ -2,8 +2,7 @@ var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var Redis = require('ioredis');
 var subscriber = new Redis();
-var redis = new Redis();
-var pastMessages = 'saved';
+var notifications = require('./notifications.js');
 
 app.listen(3000, function(){
     console.log('Listening on Port 3000');
@@ -20,7 +19,10 @@ io.on('connection', function(socket){
   var visitor = socket.client.conn.remoteAddress;
   var origin = socket.handshake.headers.origin;
   console.log("New client: %s from %s", visitor, origin);
-  updateClientWithMessages();
+
+  socket.on('fetch', function(employee){
+    notifications.fetch(socket, employee);
+  });
 });
 
 
@@ -29,26 +31,6 @@ subscriber.psubscribe('*', function(err, count) {
 });
 
 subscriber.on('pmessage', function(subscribed, channel, message) {
-    saveMessagetoRedis(message);
-    emitMessage(channel, message);
+  notifications.register(io, channel, message);
 });
 
-function updateClientWithMessages() {
-  redis.lrange(pastMessages, 0, 4).then(function(data) {
-    var length = data.length -1;
-    for (var i = 0; i < length; i++) {
-      emitMessage(pastMessages, data[i]);
-    };
-  });
-}
-
-function saveMessagetoRedis(message) {
-  redis.lpush(pastMessages, message);
-  redis.ltrim(pastMessages, 0, 19);
-}
-
-function emitMessage(channel, message) {
-  message = JSON.parse(message);
-  console.log("Emitting on (%s): %s", channel, message.event);
-  io.emit(channel, message);
-}
