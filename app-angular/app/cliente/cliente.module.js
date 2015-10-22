a9edae4d32f4bcf5cc5e3a56c4bd26511b69a021
Juan Carlos López <j.carloslopezl@hotmail.com>
2015-10-22 12:20:06 -0500

    Se cambian tabs

---
+++ app-angular/app/cliente/cliente.module.js
@@ -0,0 +1,10 @@
+// app/cliente/cliente.module.js
+
+(function() {
+  'use strict';
+
+  angular.module('sagdApp.cliente', [
+    'sagdApp.core',
+    'blocks.api'
+  ]);
+})();
--- app-angular/app/cliente/show/clienteShow.controller.js
+++ app-angular/app/cliente/show/clienteShow.controller.js
@@ -6,9 +6,11 @@
 
   angular
     .module('sagdApp.cliente')
-    .controller('clienteShowController', ClienteShowController);
+    .controller('clienteShowController', ClienteShowController)
+    .controller('tabController', TabController);
 
   ClienteShowController.$inject = ['$auth', '$state', '$stateParams', 'api'];
+  TabController.$inject = [];
 
   function ClienteShowController($auth, $state, $stateParams, api){
     if (!$auth.isAuthenticated()) {
@@ -19,99 +21,12 @@
     vm.id = $stateParams.id;
     vm.back = goBack;
 
-    vm.fields = [
-      {
-        type: 'input',
-        key: 'usuario',
-        templateOptions: {
-          label: 'Usuario',
-          placeholder: 'Introduzca el usuario',
-          required: true
-        }
-      },{
-        type: 'input',
-        key: 'nombre',
-        templateOptions: {
-          label: 'Nombre',
-        }
-      }, {
-        type: 'select',
-        key: 'sexo',
-        templateOptions: {
-          label: 'Sexo:',
-          options: [
-            {value: "HOMBRE", name: "Hombre"},
-            {value: "MUJER", name: "Mujer"}
-          ]
-        }
-      }, {
-        type: 'select',
-        key: 'cliente_referencia_id',
-        templateOptions: {
-          label: 'Referencia:',
-          required: true,
-          options: [],
-          ngOptions: 'clientes_referencias.id as clientes_referencias.nombre for clientes_referencias in to.options'
-        },
-        controller: /* @ngInject */ function ($scope){
-          $scope.to.loading = api.get('/cliente-referencia').then(function (response){
-            $scope.to.options = response.data;
-            return response;
-          });
-        }
-      }, {
-        type: 'select',
-        key: 'rol_id',
-        templateOptions: {
-          label: 'Rol:',
-          required: true,
-          options: [],
-          ngOptions: 'roles.id as roles.nombre for roles in to.options'
-        },
-        controller: /* @ngInject */ function ($scope){
-          $scope.to.loading = api.get('/rol').then(function (response){
-            $scope.to.options = response.data;
-            return response;
-          });
-        }
-      }, {
-        type: 'select',
-        key: 'cliente_estatus_id',
-        templateOptions: {
-          label: 'Estatus:',
-          required: true,
-          options: [],
-          ngOptions: 'clientes_estatus.id as clientes_estatus.nombre for clientes_estatus in to.options'
-        },
-        controller: /* @ngInject */ function ($scope){
-          $scope.to.loading = api.get('/cliente-estatus').then(function (response){
-            $scope.to.options = response.data;
-            return response;
-          });
-        }
-      }, {
-        type: 'select',
-        key: 'sucursal_id',
-        templateOptions: {
-          label: 'Sucursal de preferencia:',
-          required: true,
-          options: [],
-          ngOptions: 'sucursales.id as sucursales.nombre for sucursales in to.options'
-        },
-        controller: /* @ngInject */ function ($scope){
-          $scope.to.loading = api.get('/sucursal').then(function (response){
-            $scope.to.options = response.data;
-            return response;
-          });
-        }
-      }
-    ];
+
     initialize();
 
     function initialize(){
       return obtenerCliente().then(function (response){
         console.log(response.message);
-        $state.go('clienteShow.details');
       });
     }
 
@@ -130,6 +45,10 @@
     function goBack() {
       window.history.back();
     }
+  }
+
+  function TabController(){
+    console.log("Tab");
   }
 
 })
