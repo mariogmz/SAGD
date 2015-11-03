74d21074bb8a4f45bde465cf0a12f8de94f1435c
Omar Garcia <ogarcia@zegucom.com.mx>
2015-11-03 13:52:55 -0600

    Mostar de mejor manera los botones

--- app-angular/app/permiso/roles/roles.controller.js
+++ app-angular/app/permiso/roles/roles.controller.js
@@ -20,9 +20,13 @@
     vm.rolSeleccionado = [];
     vm.roles = [];
     vm.permisos = [];
-    vm.acciones = [];
+    vm.permisosDisponibles = [];
     vm.selectModule = selectController;
     vm.selectRole = selectRole;
+    vm.attach = attachToRole;
+    vm.detach = detachFromRole;
+    vm.able = isAble;
+    vm.notAble = isntAble;
 
     activate();
 
@@ -48,21 +52,55 @@
       })
     }
 
-    function selectController(controlador) {
-      vm.controladorSeleccionado = controlador;
-      if (vm.rolSeleccionado.length === 0) { return };
+    function attachToRole(permiso) {
+      vm.rolSeleccionado.permisos.push(permiso);
+    }
 
-      vm.acciones = [];
-      for (var i = vm.permisos.length - 1; i >= 0; i--) {
-        var permisoActual = vm.permisos[i];
-        if (permisoActual.controlador === vm.controladorSeleccionado) {
-          vm.acciones.push(permisoActual);
+    function detachFromRole(permiso) {
+      for (var i = vm.rolSeleccionado.permisos.length - 1; i >= 0; i--) {
+        var permisoRol = vm.rolSeleccionado.permisos[i];
+        if (permisoRol.id === permiso.id) {
+          vm.rolSeleccionado.permisos.splice(i,1);
+          return;
         };
       };
     }
 
     function selectRole(role) {
       vm.rolSeleccionado = role;
+      selectController(vm.controladorSeleccionado);
+    }
+
+    function selectController(controlador) {
+      if (controlador === "") { return };
+      if (vm.rolSeleccionado.length === 0) { return };
+
+      vm.controladorSeleccionado = controlador;
+      setPermisosDisponibles();
+    }
+
+    function setPermisosDisponibles(){
+      vm.permisosDisponibles = [];
+      for (var i = vm.permisos.length - 1; i >= 0; i--) {
+        var permisoActual = vm.permisos[i];
+        if (permisoActual.controlador === vm.controladorSeleccionado) {
+          vm.permisosDisponibles.push(permisoActual);
+        };
+      };
+    }
+
+    function isAble(permiso) {
+      for (var i = vm.rolSeleccionado.permisos.length - 1; i >= 0; i--) {
+        var permisoRol = vm.rolSeleccionado.permisos[i];
+        if (permisoRol.id === permiso.id) {
+          return true;
+        }
+      };
+      return false;
+    }
+
+    function isntAble(permiso) {
+      return !isAble(permiso);
     }
   }
 })();
