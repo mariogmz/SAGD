1b53478758e1c75e850d06ffa2ca81f7806c46be
Omar Garcia <ogarcia@zegucom.com.mx>
2015-11-04 10:38:46 -0600

    Agregar y quitar roles localmente

--- app-angular/app/empleado/roles/roles.controller.js
+++ app-angular/app/empleado/roles/roles.controller.js
@@ -17,14 +17,24 @@
 
     var vm = this;
     vm.empleadoId = $stateParams.empleado;
+    vm.empleado = [];
+    vm.roles = [];
+    vm.rolesEmpleado = [];
+    vm.empleadoHas = empleadoHas;
+    vm.empleadoDoesntHave = empleadoDoesntHave;
+    vm.attach = attachLocal;
+    vm.detach = detachLocal;
 
     activate();
 
     ////////////////
 
     function activate() {
-      obtenerEmpleado();
-      obtenerRoles();
+      obtenerEmpleado().then(function() {
+        obtenerRoles().then(function() {
+          obtenerRolesDeEmpleado(vm.empleadoId);
+        });
+      });
     }
 
     function obtenerEmpleado() {
@@ -37,6 +47,38 @@
       return api.get('/rol').then(function(response) {
         vm.roles = response.data;
       });
+    }
+
+    function obtenerRolesDeEmpleado(empleadoId) {
+      return api.get('/empleado/'+empleadoId+'/roles').then(function(response) {
+        vm.rolesEmpleado = response.data.roles;
+      });
+    }
+
+    function attachLocal(rol) {
+      vm.rolesEmpleado.push(rol);
+    }
+
+    function detachLocal(rol) {
+      for (var i = vm.rolesEmpleado.length - 1; i >= 0; i--) {
+        if (vm.rolesEmpleado[i].id === rol.id){
+          vm.rolesEmpleado.splice(i,1);
+          return;
+        }
+      };
+    }
+
+    function empleadoHas(rol) {
+      for (var i = vm.rolesEmpleado.length - 1; i >= 0; i--) {
+        if (vm.rolesEmpleado[i].id === rol.id) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    function empleadoDoesntHave(rol) {
+      return !empleadoHas(rol);
     }
   }
 })();
