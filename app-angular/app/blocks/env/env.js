464c2e50f95ff3a977147b865855e6c30b03afb0
Omar Garcia <ogarcia@zegucom.com.mx>
2015-11-09 11:08:30 -0600

    hotfix/ agregar al gitignore el env.js para feature de ssh tasks

--- app-angular/app/blocks/env/env.js
+++
@@ -1,8 +0,0 @@
-(function (){
-  "use strict";
-
- angular.module('blocks.env', [])
-
-.constant('ENV', {name:'development',applicationFqdn:'http://api.sagd.app',apiNamespace:'/api',version:'/v1',cache_time:1,cache_whitelist:['codigo_postal','domicilio','familia','garantia','marca','margen','proveedor','subfamilia','sucursal?proveedor_clave=DICO','unidad'],socketEndpoint:'ws://socket.sagd.app'})
-
-; }());
