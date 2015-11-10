861eb431bf71de75c1bc059290acd723e42f5174
Omar Garcia <ogarcia@zegucom.com.mx>
2015-11-10 15:03:56 -0600

    Agregar el modulo de salidas con el index

---
+++ app-angular/app/salidas/index/index.controller.js
@@ -0,0 +1,77 @@
+// app/salida/index/index.controller.js
+
+(function() {
+  'use strict';
+
+  angular
+    .module('sagdApp.salida')
+    .controller('salidaIndexController', salidaIndexController);
+
+  salidaIndexController.$inject = ['api', 'pnotify', 'modal'];
+
+  /* @ngInject */
+  function salidaIndexController(api, pnotify, modal) {
+
+    var vm = this;
+    vm.sort = sort;
+    vm.eliminarUnidad = eliminar;
+    vm.sortKeys = [
+      {name: '#', key: 'id'},
+      {name: 'Fecha', key: 'clave'},
+      {name: 'Motivo', key: 'nombre'},
+      {name: 'Empleado', key: 'nombre'},
+      {name: 'Estado', key: 'nombre'},
+    ];
+
+    activate();
+
+    ////////////////
+
+    function activate() {
+      return obtenerSalidas().then(function() {
+        console.log('Unidades obtenidas');
+      });
+    }
+
+    function obtenerSalidas() {
+      return api.get('/salida').then(function(response) {
+          vm.salidas = response.data;
+          return vm.salidas;
+        });
+    }
+
+    function eliminar(salida) {
+      modal.confirm({
+        title: 'Eliminar Salida',
+        content: 'Estas a punto de eliminar una salida. ¿Estás seguro?',
+        accept: 'Eliminar Salida',
+        type: 'danger'
+      })
+      .then(function(response) {
+        modal.hide('confirm');
+        eliminarSalida(salida.id);
+      })
+      .catch(function(response) {
+        modal.hide('confirm');
+        return false;
+      });
+    }
+
+    function eliminarSalida(id) {
+      return api.delete('/unidad/', id)
+        .then(function(response) {
+          obtenerSalidas().then(function() {
+            pnotify.alert('¡Éxito!', response.data.message, 'success');
+          });
+        })
+        .catch(function(response) {
+          pnotify.alert('¡Error!', response.data.message, 'error');
+        });
+    }
+
+    function sort(keyname) {
+      vm.sortKey = keyname;
+      vm.reverse = !vm.reverse;
+    }
+  }
+})();
