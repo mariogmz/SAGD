f75676be37a45b5e66f72d6344d03195a368a2e7
Omar Garcia <ogarcia@zegucom.com.mx>
2015-11-10 15:37:31 -0600

    Cambios menores al controlador de salidas

--- app/Http/Controllers/Api/V1/SalidaController.php
+++ app/Http/Controllers/Api/V1/SalidaController.php
@@ -2,30 +2,30 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use Illuminate\Http\Request;
-use App\Http\Requests;
+use App\Salida;
 use App\Http\Controllers\Controller;
+use App\Http\Requests;
+use Illuminate\Http\Request;
 
 class SalidaController extends Controller
 {
+    protected $salida;
+
+    public function __construct(Salida $salida)
+    {
+        $this->salida = $salida;
+        $this->middleware('jwt.auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Salida $salida)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $this->authorize($this);
+        return $this->salida->all();
     }
 
     /**
@@ -36,7 +36,24 @@ class SalidaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->authorize($this);
+        $params = $request->all();
+        $this->salida->fill($params);
+        if( $this->salida->save() )
+        {
+            return response()->json(
+                [
+                    'message' => 'Salida creada exitosamente',
+                    'salida' => $this->salida->self()
+                ],
+                201,
+                ['Location' => route('api.v1.salida.show', $this->salida->getId())]);
+        } else {
+            return response()->json([
+                'message' => 'Salida no creada',
+                'error' => $this->salida->errors
+            ], 400);
+        }
     }
 
     /**
@@ -47,18 +64,20 @@ class SalidaController extends Controller
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        $this->authorize($this);
+        $this->salida = $this->salida->find($id);
+        if( $this->salida )
+        {
+            return response()->json([
+                'message' => 'Salida obtenida exitosamente',
+                'salida' => $this->salida
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Salida no encontrada o no existente',
+                'error' => 'No encontrada'
+            ], 404);
+        }
     }
 
     /**
@@ -70,7 +89,27 @@ class SalidaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->authorize($this);
+        $parameters = $request->all();
+        $this->salida = $this->salida->find($id);
+        if( empty($this->salida) )
+        {
+            return response()->json([
+                'message' => 'No se pudo realizar la actualizacion de la salida',
+                'error' => 'Salida no encontrada'
+            ], 404);
+        }
+        if( $this->salida->update($parameters) )
+        {
+            return response()->json([
+                'message' => 'Salida se actualizo correctamente'
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'No se pudo realizar la actualizacion de la salida',
+                'error' => $this->salida->errors
+            ], 400);
+        }
     }
 
     /**
@@ -81,6 +120,25 @@ class SalidaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $this->authorize($this);
+        $this->salida = $this->salida->find($id);
+        if( empty($this->salida) )
+        {
+            return response()->json([
+                'message' => 'No se pudo eliminar la salida',
+                'error' => 'Salida no encontrada'
+            ], 404);
+        }
+        if( $this->salida->delete() )
+        {
+            return response()->json([
+                'message' => 'Salida eliminada correctamente'
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'No se pudo eliminar la salida',
+                'error' => 'El metodo de eliminar no se pudo ejecutar'
+            ], 400);
+        }
     }
 }
