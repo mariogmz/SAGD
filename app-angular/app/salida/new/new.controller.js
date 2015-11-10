aa94a34ba972fbc15cb65f42b37efec56902d9ca
Omar Garcia <ogarcia@zegucom.com.mx>
2015-11-10 17:26:31 -0600

    Salida new controller basico

--- app-angular/app/salida/new/new.controller.js
+++ app-angular/app/salida/new/new.controller.js
@@ -7,38 +7,26 @@
     .module('sagdApp.salida')
     .controller('salidaNewController', salidaNewController);
 
-  salidaNewController.$inject = ['$state', 'api', 'pnotify'];
+  salidaNewController.$inject = ['$state', 'api', 'pnotify', 'utils'];
 
   /* @ngInject */
-  function salidaNewController($state, api, pnotify) {
+  function salidaNewController($state, api, pnotify, utils) {
 
     var vm = this;
 
     vm.create = create;
+    vm.setClass = utils.setClass;
     vm.back = goBack;
-
-    vm.fields = [
-      {
-        type: 'input',
-        key: 'motivo',
-        templateOptions: {
-          type: 'text',
-          label: 'Motivo:',
-          placeholder: '¿Porqué se realiza esta salida?',
-          required: true,
-          maxlength: 140
-        },
-      }, {
-        type: 'input',
-        key: 'fecha_salida',
-        defaultValue: new Date(),
-        templateOptions: {
-          type: 'date',
-          label: 'Fecha:',
-          maxlength: 45
+    vm.agregarSalidaDetalle = agregarSalidaDetalle;
+    vm.salida = {
+      fecha_salida: new Date(),
+      salidas_detalles: [
+        {
+          cantidad: 0,
+          upc: ''
         }
-      }
-    ];
+      ]
+    };
 
     activate();
 
@@ -56,6 +44,25 @@
         .catch(function(response) {
           pnotify.alertList(response.data.message, response.data.error, 'error');
         });
+    }
+
+    function agregarSalidaDetalle() {
+      checkIfSalidasDetallesHasEmptySets() && vm.salida.salidas_detalles.push({
+        cantidad: 0,
+        upc: ''
+      });
+    }
+
+    function checkIfSalidasDetallesHasEmptySets() {
+      for (var i = vm.salida.salidas_detalles.length - 1; i >= 0; i--) {
+        if (vm.salida.salidas_detalles[i].upc === '') {
+          return false;
+        } else if (vm.salida.salidas_detalles[i].cantidad === 0) {
+          return false;
+        }
+      }
+
+      return true;
     }
 
     function goBack() {
