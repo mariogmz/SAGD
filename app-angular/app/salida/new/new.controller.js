e7a94945402bf0770c28ce2b92b365b262e748db
Omar Garcia <ogarcia@zegucom.com.mx>
2015-11-18 11:31:55 -0600

    Creaciones de salidas

--- app-angular/app/salida/new/new.controller.js
+++ app-angular/app/salida/new/new.controller.js
@@ -7,10 +7,10 @@
     .module('sagdApp.salida')
     .controller('salidaNewController', salidaNewController);
 
-  salidaNewController.$inject = ['$state', 'api', 'pnotify', 'utils'];
+  salidaNewController.$inject = ['$state', 'api', 'pnotify', 'utils', 'session'];
 
   /* @ngInject */
-  function salidaNewController($state, api, pnotify, utils) {
+  function salidaNewController($state, api, pnotify, utils, session) {
 
     var vm = this;
 
@@ -18,11 +18,14 @@
     vm.setClass = utils.setClass;
     vm.back = goBack;
     vm.agregarSalidaDetalle = agregarSalidaDetalle;
-    vm.isValid = isValid;
-    vm.isInvalid = isInvalid;
+    vm.removerSalidaDetalle = removerSalidaDetalle;
+    vm.empleado = session.obtenerEmpleado();
     vm.salida = {
       fecha_salida: new Date(),
-      salidas_detalles: []
+      salidas_detalles: [],
+      estado_salida_id: 1,
+      empleado_id: vm.empleado.id,
+      sucursal_id: vm.empleado.sucursal_id
     };
 
     activate();
@@ -33,6 +36,7 @@
     }
 
     function create() {
+      (vm.salida.salidas_detalles.length > 0) &&
       api.post('/salida', vm.salida)
         .then(function(response) {
           pnotify.alert('Exito', response.data.message, 'success');
@@ -43,37 +47,41 @@
         });
     }
 
-    function agregarSalidaDetalle() {
-      checkIfSalidasDetallesHasEmptySets() && vm.salida.salidas_detalles.push({
-        cantidad: 0,
-        upc: ''
-      });
+    function agregarSalidaDetalle(salidaDetalle) {
+      checkIsSalidaDetalleIsValid(salidaDetalle) && appendSalidaDetalle(salidaDetalle);
     }
 
-    function checkIfSalidasDetallesHasEmptySets() {
-      for (var i = vm.salida.salidas_detalles.length - 1; i >= 0; i--) {
-        if (vm.salida.salidas_detalles[i].upc === '') {
-          return false;
-        } else if (vm.salida.salidas_detalles[i].cantidad === 0) {
-          return false;
-        }
-      }
-
-      return true;
-    }
-
-    function isValid(salidaDetalle) {
-      if (salidaDetalle.cantidad <= 0) {
+    function checkIsSalidaDetalleIsValid(salidaDetalle) {
+      if (salidaDetalle.cantidad <= 0 || salidaDetalle.upc.length <= 0) {
         return false;
       }
 
       return true;
-
-      // TODO checar en la API si la el producto con el UPC existe y si las existencias son mayores a cantidad
     }
 
-    function isInvalid(salidaDetalle) {
-      return !isValid(salidaDetalle);
+    function appendSalidaDetalle(salidaDetalle) {
+      buscarProducto(salidaDetalle.upc).then(function(response) {
+        vm.salida.salidas_detalles.push({
+          cantidad: salidaDetalle.cantidad,
+          producto: response.data.producto
+        });
+      }).catch(function(response) {
+        pnotify.alert('Detalle no agregado', response.data.error, 'error');
+      });
+    }
+
+    function buscarProducto(upc) {
+      return api.get('/producto/buscar/upc/', upc);
+    }
+
+    function removerSalidaDetalle(salidaDetalle) {
+      for (var i = vm.salida.salidas_detalles.length - 1; i >= 0; i--) {
+        var sd = vm.salida.salidas_detalles[i];
+        if (sd === salidaDetalle) {
+          vm.salida.salidas_detalles.splice(i, 1);
+          break;
+        }
+      }
     }
 
     function goBack() {
