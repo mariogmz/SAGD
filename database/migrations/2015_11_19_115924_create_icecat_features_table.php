f285685d707898dc561e1fdc42813ce1245438b5
Mario Gómez <mariogomezmtz@gmail.com>
2015-11-19 12:52:32 -0600

    Agregar migracion, pruebas y modelo de IcecatFeature

---
+++ database/migrations/2015_11_19_115924_create_icecat_features_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateIcecatFeaturesTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('icecat_features', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('icecat_id')->unsigned();
+            $table->string('type', 45)->nullable()->default('text');
+            $table->string('name', 70);
+            $table->string('description', 100)->nullable();
+            $table->string('measure', 10)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('icecat_features');
+    }
+}
