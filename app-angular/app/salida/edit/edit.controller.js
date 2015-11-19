b117267a6b47a736275849cf381d63b92f4297a9
Omar Garcia <ogarcia@zegucom.com.mx>
2015-11-19 10:39:25 -0600

    Quitar de la BD cuando se elimina el detalle

--- app-angular/app/salida/edit/edit.controller.js
+++ app-angular/app/salida/edit/edit.controller.js
@@ -65,7 +65,7 @@
       buscarProducto(salidaDetalle.upc).then(function(responseProducto) {
         salidaDetalle.producto_id = responseProducto.data.producto.id;
         saveSalidaDetalle(salidaDetalle).then(function(responseDetalle) {
-          vm.salida.salidas_detalles.push({
+          vm.salida.detalles.push({
             cantidad: salidaDetalle.cantidad,
             producto: responseProducto.data.producto
           });
@@ -84,13 +84,19 @@
     }
 
     function removerSalidaDetalle(salidaDetalle) {
-      for (var i = vm.salida.salidas_detalles.length - 1; i >= 0; i--) {
-        var sd = vm.salida.salidas_detalles[i];
-        if (sd === salidaDetalle) {
-          vm.salida.salidas_detalles.splice(i, 1);
-          break;
+      unsaveSalidaDetalle(salidaDetalle).then(function(response) {
+        for (var i = vm.salida.detalles.length - 1; i >= 0; i--) {
+          var sd = vm.salida.detalles[i];
+          if (sd === salidaDetalle) {
+            vm.salida.detalles.splice(i, 1);
+            break;
+          }
         }
-      }
+      });
+    }
+
+    function unsaveSalidaDetalle(salidaDetalle) {
+      return api.delete('/salida/' + vm.salida.id + '/detalles/' + salidaDetalle.id);
     }
 
     function goBack() {
