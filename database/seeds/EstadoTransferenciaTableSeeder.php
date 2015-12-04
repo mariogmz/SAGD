f9f564ec7a87be6d6bbe71260db5680823663b44
Omar Garcia <ogarcia@zegucom.com.mx>
2015-12-04 12:39:04 -0600

    Establecer estados de transferencia con proceso v1

--- database/seeds/EstadoTransferenciaTableSeeder.php
+++ database/seeds/EstadoTransferenciaTableSeeder.php
@@ -11,21 +11,39 @@ class EstadoTransferenciaTableSeeder extends Seeder {
      */
     public function run() {
         /**
-         * Definicion de status:
-         * 1 => Abierta.                    Puede editarse y cargarse
-         * 2 => Cargando Local.             En proceso de carga, no puede editarse
-         * 3 => Cargada Local.              La carga se realizo con exito, no puede editarse
-         * 4 => Iniciando Transferencia.
-         * 5 => Transferencia Terminada.    Lista Para Cargarse Sucursal Destino
-         * 6 => Cargando Otra Sucursal.     En proceso de carga en otra sucursal
-         * 7 => Cargada Otra Sucursal.      Indica que se realizo la carga exitosamente
+         * Definicion de estados:
+         *
+         * 1 => Abierta
+         * Se generó una pretransferencia y se imprimió la orden.
+         * La transferencia es editable en todo excepto sucursales.
+         *
+         * 2 => Cargando Origen
+         * Se empezaron a agregar detalles a la transferencia, es decir, se
+         * estan escaneando los productos que van de salida.
+         * La transferencia ya tiene quien la origino (el que está cargando
+         * productos).
+         *
+         * 3 => En transferencia
+         * Se marcó como cargada la transferencia. El proceso físico de enviar
+         * al destino ocurre aquí. La transferencia no es editable en este
+         * estado y el producto esta marcado como cantidad_transferencia en la
+         * sucursal de origen.
+         *
+         * 4 => Cargando Destino
+         * La mercancía (transferencia) llegó al destino y se encuentran
+         * escaneando los productos para verificar que si llegaron todos.
+         *
+         * 5 => Finalizada
+         * Cuando todos los productos de la transferencia esten contados y
+         * verificados y las existencias se eliminaron del origen y ahora
+         * se encuentran en las existencias del destino.
+         * Es inmutable ambos transferencias y sus detalles ya que es el estado
+         * final.
          */
         App\EstadoTransferencia::create(['nombre' => 'Abierta']);
-        App\EstadoTransferencia::create(['nombre' => 'Cargando Local']);
-        App\EstadoTransferencia::create(['nombre' => 'Cargada Local']);
-        App\EstadoTransferencia::create(['nombre' => 'Iniciando Transferencia']);
-        App\EstadoTransferencia::create(['nombre' => 'Transferencia Terminada']);
-        App\EstadoTransferencia::create(['nombre' => 'Cargando Otra Sucursal']);
-        App\EstadoTransferencia::create(['nombre' => 'Cargada Otra Sucursal']);
+        App\EstadoTransferencia::create(['nombre' => 'Cargando Origen']);
+        App\EstadoTransferencia::create(['nombre' => 'En transferencia']);
+        App\EstadoTransferencia::create(['nombre' => 'Cargando Destino']);
+        App\EstadoTransferencia::create(['nombre' => 'Finalizada']);
     }
 }
