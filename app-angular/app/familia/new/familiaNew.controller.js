e56e3ae07fcd8437a1023ff3784975184cd7b7c6
Mario Gómez <mariogomezmtz@gmail.com>
2015-12-14 15:43:59 -0600

    Fix en número de caracteres permitidos para clave en nueva Familia.

--- app-angular/app/familia/new/familiaNew.controller.js
+++ app-angular/app/familia/new/familiaNew.controller.js
@@ -1,6 +1,6 @@
 // app/familia/familia.controller.js
 
-(function (){
+(function() {
 
   'use strict';
 
@@ -10,7 +10,7 @@
 
   FamiliaNewController.$inject = ['$state', 'api', 'pnotify'];
 
-  function FamiliaNewController($state, api, pnotify){
+  function FamiliaNewController($state, api, pnotify) {
 
     var vm = this;
     vm.back = goBack;
@@ -21,15 +21,16 @@
         templateOptions: {
           type: 'text',
           label: 'Clave:',
-          placeholder: 'Máximo 5 caracteres alfanuméricos',
+          placeholder: 'Máximo 4 caracteres alfanuméricos',
           required: true,
-          maxlength: 5
+          maxlength: 4
         },
         validators: {
           validKey: {
-            expression: function ($viewValue, $modelValue, scope){
+            expression: function($viewValue, $modelValue, scope) {
               return /^[\w]+$/.test($viewValue || $modelValue);
             },
+
             message: '$viewValue + " contiene caracteres inválidos"'
           }
         }
@@ -55,18 +56,18 @@
 
     vm.create = create;
 
-    function create(){
+    function create() {
       api.post('/familia', vm.familia)
-        .then(function (response){
+        .then(function(response) {
           pnotify.alert('¡Exito!', response.data.message, 'success');
           $state.go('familiaShow', {id: response.data.familia.id});
         })
-        .catch(function (response){
+        .catch(function(response) {
           pnotify.alertList(response.data.message, response.data.error, 'error');
         });
     }
 
-    function goBack(){
+    function goBack() {
       window.history.back();
     }
   }
