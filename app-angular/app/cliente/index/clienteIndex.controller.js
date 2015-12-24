95bb85979e1262e9a6d907680f8871c3b60e7e75
Mario Gómez <mariogomezmtz@gmail.com>
2015-12-24 13:29:00 -0600

    Agregar index para clientes.

--- app-angular/app/cliente/index/clienteIndex.controller.js
+++ app-angular/app/cliente/index/clienteIndex.controller.js
@@ -1,56 +1,80 @@
 // app/cliente/index/clienteIndex.controller.js
 
-(function () {
-    'use strict';
+(function() {
+  'use strict';
 
-    angular
-        .module('sagdApp.cliente')
-        .controller("clienteIndexController", ClienteIndexController);
+  angular
+    .module('sagdApp.cliente')
+    .controller('clienteIndexController', ClienteIndexController);
 
-    ClienteIndexController.$inject = ['api', 'pnotify'];
+  ClienteIndexController.$inject = ['api', 'pnotify'];
 
-    function ClienteIndexController($auth, $state, api, pnotify) {
+  /* @ngInject */
+  function ClienteIndexController(api, pnotify) {
+    var vm = this;
+    vm.sort = sort;
+    vm.eliminarProducto = eliminarCliente;
 
-        var vm = this;
-        vm.eliminarCliente = eliminarCliente;
-        vm.sort = sort;
-        vm.sortKeys = [
-            {name: '#', key: 'id'},
-            {name: 'Usuario', key: 'usuario'},
-            {name: 'Nombre', key: 'nombre'},
-            {name: 'Estatus', key: 'estatus.id'},
-        ];
+    vm.searching = false;
+    vm.delete = eliminarCliente;
+    vm.buscar = buscar;
 
-        vm.obtenerClientes = function () {
-            api.get('/cliente').
-                then(function (response) {
-                    vm.clientes = response.data;
-                }, function (response) {
-                    vm.errors = response;
+    initialize();
 
-                });
-        };
-
-        vm.obtenerClientes();
-
-        function eliminarCliente(id) {
-            return api.delete('/cliente/', id)
-                .then(function(response){
-                    obtenerClientes()
-                        .then(function(){
-                            pnotify.alert('Exito', response.data.message, 'success');
-                        });
-                })
-                .catch(function(response){
-                    pnotify.alert('Error', response.data.message, 'error');
-                });
-        }
-
-        function sort(keyname){
-            vm.sortKey = keyname;
-            vm.reverse = !vm.reverse;
-        }
-
+    function initialize() {
+      vm.sortKeys = [
+        {name: '#', key: 'id'},
+        {name: 'Nombre', key: 'nombre'},
+        {name: 'Usuario', key: 'usuario'},
+        {name: 'Email', key: 'email'}
+      ];
+      vm.search = {
+        nombre: '',
+        usuario: '',
+        email: ''
+      };
     }
+
+    function buscar() {
+      vm.searching = !vm.searching;
+      obtenerClientes().then(success).catch(error);
+    }
+
+    function obtenerClientes() {
+      return api.get('/clientes/buscar/', vm.search);
+    }
+
+    function eliminarCliente(id) {
+      return api.delete('/cliente/', id)
+        .then(function(response) {
+          obtenerClientes().then(function() {
+            pnotify.alert('¡Exito!', response.data.message, 'success');
+          });
+        }).catch(function(response) {
+          pnotify.alert('¡Error!', response.data.message, 'error');
+        });
+    }
+
+    function sort(keyname) {
+      vm.sortKey = keyname;
+      vm.reverse = !vm.reverse;
+    }
+
+    function success(response) {
+      vm.searching = !vm.searching;
+      vm.clientes = response.data.map(function(cliente) {
+        cliente.email = cliente.user ? cliente.user.email : '';
+        return cliente;
+      });
+
+      return response;
+    }
+
+    function error(response) {
+      vm.searching = !vm.searching;
+      pnotify.alert(response.data.error, response.data.message, 'error');
+    }
+
+  }
 
 })();
