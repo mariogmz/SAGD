21a7adc9dc3a30bd9e959d857691a88fffee85ea
Mario Gómez <mariogomezmtz@gmail.com>
2016-01-05 11:14:35 -0600

    Refactorizar UX para directiva de domicilios.

--- app-angular/app/blocks/domicilio/domicilio.directive.js
+++ app-angular/app/blocks/domicilio/domicilio.directive.js
@@ -34,21 +34,36 @@
 
     var vm = this;
     vm.selectDomicilio = selectDomicilio;
+    vm.selectTelefono = selectTelefono;
     vm.selectCp = selectCp;
+    vm.selectTipo = selectTipo;
+    vm.agregarDomicilio = agregarDomicilio;
+    vm.agregarTelefono = agregarTelefono;
+    vm.removerDomicilio = removerDomicilio;
+    vm.removerTelefono = removerTelefono;
+    vm.setUpdateDomicilio = setUpdateDomicilio;
+    vm.setUpdateTelefono = setUpdateTelefono;
+    vm.visible = visible;
     activate();
+
+    /**
+     * Definición de acciones para cada record en las colecciones.
+     * Propiedad 'action' en cada objeto de domicilio o teléfono
+     * 0 -> Crear nuevo
+     * 1 -> Actualizar
+     * 2 -> Eliminar
+     * Cualquier otro -> No hacer nada
+     */
 
     ////////////////////////
 
     function activate() {
       vm.domicilios = vm.cliente.domicilios;
       vm.tipos = [{tipo: 'FIJO'}, {tipo: 'CELULAR'}, {tipo: 'FAX'}];
+
       getCodigosPostales().then(function(response) {
         return response;
       });
-    }
-
-    function selectDomicilio(domicilio) {
-      vm.domicilio = domicilio;
     }
 
     function getCodigosPostales() {
@@ -59,17 +74,107 @@
         });
     }
 
+    function selectDomicilio(domicilio) {
+      vm.domicilio = domicilio;
+      vm.telefono = ultimoSeleccionable(vm.domicilio.telefonos || []);
+    }
+
+    function selectTelefono(telefono) {
+      vm.telefono = telefono;
+    }
+
     function selectCp($item) {
       if ($item) {
-        vm.domicilios[this.$parent.$index].codigoPostal = {
-          estado: $item.originalObject.estado,
-          municipio: $item.originalObject.municipio
-        };
+        vm.domicilio.codigo_postal = $item.originalObject;
+        vm.domicilio.codigo_postal_id = $item.originalObject.id;
       } else {
-        vm.domicilios[this.$parent.$index].codigoPostal = null;
+        vm.domicilio.codigo_postal = null;
+        vm.domicilio.codigo_postal_id = null;
       }
+    }
+
+    function selectTipo($item) {
+      if ($item) {
+        vm.telefono.tipo = $item.title;
+      } else {
+        vm.telefono.tipo = null;
+      }
+    }
+
+    function agregarDomicilio() {
+      if (!vm.domicilios) {
+        vm.domicilios = [];
+      }
+
+      vm.domicilios.push({
+        action: 0
+      });
+      vm.domicilio = vm.domicilios[vm.domicilios.length - 1];
+    }
+
+    function agregarTelefono() {
+      if (!vm.domicilio.telefonos) {
+        vm.domicilio.telefonos = [];
+      }
+
+      vm.domicilio.telefonos.push({
+        action: 0
+      });
+      vm.telefono = vm.domicilio.telefonos[vm.domicilio.telefonos.length - 1];
+    }
+
+    function removerDomicilio() {
+      if (vm.domicilio.action == 0) {
+        var index = vm.domicilios.indexOf(vm.domicilio);
+        vm.domicilios.splice(index, 1);
+      } else {
+        vm.domicilio.action = 2; // Acción eliminar
+      }
+
+      vm.domicilio = ultimoSeleccionable(vm.domicilios);
+    }
+
+    function removerTelefono() {
+      if (vm.telefono.action == 0) {
+        var index = vm.domicilio.telefonos.indexOf(vm.telefono);
+        vm.domicilio.telefonos.splice(index, 1);
+      } else {
+        vm.telefono.action = 2; // Acción eliminar
+      }
+
+      vm.telefono = ultimoSeleccionable(vm.domicilio.telefonos);
+    }
+
+    function setUpdateDomicilio() {
+
+    }
+
+    function setUpdateTelefono() {
+    }
+
+    ////////////////////// UTILS //////////////////////
+
+    function visible(value) {
+      if (!value.hasOwnProperty('action')) {
+        return true;
+      } else {
+        return value.action != 2;
+      }
+    }
+
+    function ultimoSeleccionable(array) {
+      for (var i = array.length - 1; i >= 0; i--) {
+        if (!array[i].hasOwnProperty('action')) {
+          return array[i];
+        } else {
+          if (array[i].action != 2) {
+            return array[i];
+          }
+        }
+      }
+
+      return null;
     }
   }
 
 })();
-
