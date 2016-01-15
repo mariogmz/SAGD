17cc0d25cac4a6b613838d46a2e145c038b22bab
Mario Gómez <mariogomezmtz@gmail.com>
2016-01-15 10:49:37 -0600

    Refactorizar nombre de módulos para resources.

--- app-angular/app/resources/codigoPostal.resource.js
+++ app-angular/app/resources/codigoPostal.resource.js
@@ -4,7 +4,7 @@
   'use strict';
 
   angular
-    .module('blocks.resources')
+    .module('sagdApp.resources')
     .factory('CodigoPostal', CodigoPostal);
 
   CodigoPostal.$inject = ['api', 'pnotify'];
--- app-angular/app/resources/domicilio.resource.js
+++ app-angular/app/resources/domicilio.resource.js
@@ -4,7 +4,7 @@
   'use strict';
 
   angular
-    .module('blocks.resources')
+    .module('sagdApp.resources')
     .factory('Domicilio', Domicilio);
 
   Domicilio.$inject = ['api', 'pnotify'];
