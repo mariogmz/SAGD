5076c789fbcae074e62197068005c252ba05485b
Omar Garcia <ogarcia@zegucom.com.mx>
2016-01-26 12:32:29 -0600

    Modificar los seeders de Icecat para usar un dump

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,62 +12,50 @@ class DatabaseSeeder extends Seeder {
      */
     public function run() {
         Model::unguard();
-        $this->call(EstatusActivoTableSeeder::class);
-        $this->call(EstadoVentaTableSeeder::class);
-        $this->call(EstatusVentaTableSeeder::class);
-        $this->call(MetodoPagoTableSeeder::class);
-        $this->call(MetodoPagoRangoTableSeeder::class);
-        $this->call(TipoPartidaTableSeeder::class);
-        $this->call(TipoVentaTableSeeder::class);
-        $this->call(EstadoFacturaTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(PermisoTableSeeder::class);
-        $this->call(CodigoPostalTableSeeder::class);
-        $this->call(ProveedorTableSeeder::class);
-        $this->call(SucursalTableSeeder::class);
-        $this->call(RazonSocialEmisorSeeder::class);
-        $this->call(TelefonoTableSeeder::class);
-        $this->call(EstadoSoporteTableSeeder::class);
-        $this->call(EstadoRmaTableSeeder::class);
-        $this->call(RmaTiempoTableSeeder::class);
-        $this->call(EstadoApartadoTableSeeder::class);
-        $this->call(EstadoEntradaTableSeeder::class);
-        $this->call(EstadoSalidaTableSeeder::class);
-        $this->call(EstadoTransferenciaTableSeeder::class);
-        $this->call(EstadoPretransferenciaSeeder::class);
-        $this->call(ClienteEstatusTableSeeder::class);
-        $this->call(ClienteReferenciaTableSeeder::class);
-        $this->call(EmpleadoTableSeeder::class);
-        $this->call(PermisosInicialesSeeder::class);
-        $this->call(ClienteModuleSeeder::class);
+        // $this->call(EstatusActivoTableSeeder::class);
+        // $this->call(EstadoVentaTableSeeder::class);
+        // $this->call(EstatusVentaTableSeeder::class);
+        // $this->call(MetodoPagoTableSeeder::class);
+        // $this->call(MetodoPagoRangoTableSeeder::class);
+        // $this->call(TipoPartidaTableSeeder::class);
+        // $this->call(TipoVentaTableSeeder::class);
+        // $this->call(EstadoFacturaTableSeeder::class);
+        // $this->call(RolesTableSeeder::class);
+        // $this->call(PermisoTableSeeder::class);
+        // $this->call(CodigoPostalTableSeeder::class);
+        // $this->call(ProveedorTableSeeder::class);
+        // $this->call(SucursalTableSeeder::class);
+        // $this->call(RazonSocialEmisorSeeder::class);
+        // $this->call(TelefonoTableSeeder::class);
+        // $this->call(EstadoSoporteTableSeeder::class);
+        // $this->call(EstadoRmaTableSeeder::class);
+        // $this->call(RmaTiempoTableSeeder::class);
+        // $this->call(EstadoApartadoTableSeeder::class);
+        // $this->call(EstadoEntradaTableSeeder::class);
+        // $this->call(EstadoSalidaTableSeeder::class);
+        // $this->call(EstadoTransferenciaTableSeeder::class);
+        // $this->call(EstadoPretransferenciaSeeder::class);
+        // $this->call(ClienteEstatusTableSeeder::class);
+        // $this->call(ClienteReferenciaTableSeeder::class);
+        // $this->call(EmpleadoTableSeeder::class);
+        // $this->call(PermisosInicialesSeeder::class);
+        // $this->call(ClienteModuleSeeder::class);
         $this->call(MarcaTableSeeder::class);
-        $this->call(TipoGarantiaTableSeeder::class);
-        $this->call(UnidadTableSeeder::class);
-        $this->call(MargenTableSeeder::class);
+        // $this->call(TipoGarantiaTableSeeder::class);
+        // $this->call(UnidadTableSeeder::class);
+        // $this->call(MargenTableSeeder::class);
         $this->call(FamiliaTableSeeder::class);
         $this->call(SubfamiliaTableSeeder::class);
-        $this->seedIcecat();
-        $this->call(PaqueteriaTableSeeder::class);
-        $this->call(ProductoTableSeeder::class);
-        $this->call(ExistenciasTableSeeder::class);
-        $this->call(PaqueteriaCoberturaTableSeeder::class);
-        $this->call(ZonaTableSeeder::class);
-        $this->call(GuiaTableSeeder::class);
-        $this->call(GuiaZonaTableSeeder::class);
-        $this->call(PaqueteriaRangoTableSeeder::class);
+        $this->call(IcecatModuleSeeder::class);
+        // $this->call(PaqueteriaTableSeeder::class);
+        // $this->call(ProductoTableSeeder::class);
+        // $this->call(ExistenciasTableSeeder::class);
+        // $this->call(PaqueteriaCoberturaTableSeeder::class);
+        // $this->call(ZonaTableSeeder::class);
+        // $this->call(GuiaTableSeeder::class);
+        // $this->call(GuiaZonaTableSeeder::class);
+        // $this->call(PaqueteriaRangoTableSeeder::class);
 
         Model::reguard();
-    }
-
-    private function seedIcecat(){
-        if($this->command->confirm('Do you want to seed Icecat database? This may take a very long time to complete.', false)){
-            $this->call(IcecatFeatureTableSeeder::class);
-            $this->call(IcecatSupplierTableSeeder::class);
-            $this->call(IcecatCategoryTableSeeder::class);
-            $this->call(IcecatFeatureGroupTableSeeder::class);
-            $this->call(IcecatCategoryFeatureGroupTableSeeder::class);
-            $this->call(IcecatCategoryFeatureTableSeeder::class);
-        }
-
     }
 }
